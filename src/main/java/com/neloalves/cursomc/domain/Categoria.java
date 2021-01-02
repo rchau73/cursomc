2f611f35225f02701de93136e98036267a1b8446
Roberto Chau <r.chau73@gmail.com>
2021-01-02 18:43:57 -0300

    DB2 e criacao do banco de dados

--- src/main/java/com/neloalves/cursomc/domain/Categoria.java
+++ src/main/java/com/neloalves/cursomc/domain/Categoria.java
@@ -8,6 +8,11 @@ import javax.persistence.GenerationType;
 
 @Entity
 public class Categoria implements Serializable {
+	public Categoria(Integer id, String nome) {
+		super();
+		Id = id;
+		this.nome = nome;
+	}
 	private static final long serialVersionUID = 1L;
 	
 	@javax.persistence.Id
@@ -26,6 +31,29 @@ public class Categoria implements Serializable {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
 	}
 		
 }
